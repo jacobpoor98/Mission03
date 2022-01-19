f78dccabf40aca9952ffd004c85a3eea7911af47
Jacob Poor <jacob.poor98@gmail.com>
2022-01-18 17:08:32 -0700

    yes

---
+++ Mission02/models/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission02.models
+{
+    public class CalculatorModel
+    {
+//        <asp:RangeValidator runat = "server" Type="Integer" 
+//MinimumValue="0" MaximumValue="400" ControlToValidate="ValueTextBox" 
+//ErrorMessage="Value must be a whole number between 0 and 400" />
+        [Required]
+        public float assignments { get; set; }
+        [Required]
+        public float groupprojects { get; set; }
+        [Required]
+        public float quizzes { get; set; }
+        [Required]
+        public float exams { get; set; }
+        [Required]
+        public float intex { get; set; }
+    }
+}
